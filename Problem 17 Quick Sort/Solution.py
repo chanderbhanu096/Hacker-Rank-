262273be9ccdee2cd6b1ccb4d28bc4441f548121
Chander Bhanu <chanderbhanu096@gmail.com>
2020-09-04 22:44:37 +0530

    quick sort

---
+++ Problem 17 Quick Sort/Solution.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the quickSort function below.
+def quickSort(arr):
+    arr.sort()
+    return arr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = quickSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
