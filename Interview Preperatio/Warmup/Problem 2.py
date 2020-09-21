398f0aade8cb503237b15f469c26ae98196e1525
Chander Bhanu <chanderbhanu096@gmail.com>
2020-09-21 22:21:53 +0530

    counting valleya

---
+++ Interview Preperatio/Warmup/Problem 2.py
@@ -0,0 +1,29 @@
+
+# Problem
+# Submissions
+# Leaderboard
+# Discussions
+# Editorial
+# An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
+
+# A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+# A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+# Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+# Example
+
+ 
+
+# The hiker first enters a valley  units deep. Then they climb out and up onto a mountain  units high. Finally, the hiker returns to sea level and ends the hike.
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+    level=valley=0
+    for i in range(n):
+        if(s[i]=='U'):
+            level+=1
+            if(level==0):
+                valley+=1
+        else:
+            level-=1
+    
+    return valley
